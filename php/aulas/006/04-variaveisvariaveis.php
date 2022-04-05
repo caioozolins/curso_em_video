77ff57b2c335c2d17022221f6b0be61856a7a742
Caio Ozolins Mendes <caio.mendes@alunos.unis.edu.br>
2022-04-05 20:16:39 -0300

    Create 04-variaveisvariaveis.php
    Aula 6 – Operadores de Atribuição

---
+++ php/aulas/006/04-variaveisvariaveis.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../styles/style.css">
+</head>
+<body>
+    <div>
+        <?php 
+        $x = "abc";
+        $$x = "def";
+        echo "O conteúdo da variável X é $x";
+        echo "<br>A variável criada ABC recebeu o valor $abc"
+        ?>
+    </div>
+</body>
+</html>
