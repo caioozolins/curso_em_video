0107e7e4b1421a4fce3fb0d1c3bf7182115054eb
Caio Ozolins Mendes <caio.mendes@alunos.unis.edu.br>
2022-04-05 20:33:09 -0300

    Create 02-igualidentico.php
    Aula 7 – Operadores Relacionais

---
+++ php/aulas/007/02-igualidentico.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../../styles/style.css">
+</head>
+
+<body>
+    <div>
+        <?php
+        $a = 3;
+        $b = "3";
+        $r = $a == $b ? "SIM" : "NÃO";
+        echo "As variáveis A e B são iguais? $r";
+        $r = $a === $b ? "SIM" : "NÃO";
+        echo "<br>As variáveis A e B são idênticas? $r";
+        ?>
+    </div>
+</body>
+
+</html>
